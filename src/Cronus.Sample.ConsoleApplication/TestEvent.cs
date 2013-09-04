78d2977bb6610bfddee4bffa31c3f62457ecaece
simeondimov <simeon.dimov@mentormate.com>
2013-09-04 14:48:53 +0300

    Added test sample project

---
+++ src/Cronus.Sample.ConsoleApplication/TestEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cronus.Core.Eventing;
+
+namespace Cronus.Sample.ConsoleApplication
+{
+    public class TestEvent : IEvent
+    {
+    }
+}
---
+++ src/Cronus.Sample.ConsoleApplication/TestProjection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cronus.Core.Eventing;
+
+namespace Cronus.Sample.ConsoleApplication
+{
+    public class TestProjection : IEventHandler,
+        IEventHandler<TestEvent>
+    {
+        public void Handle(TestEvent evnt)
+        {
+            int i = 0;
+            i++;
+        }
+    }
+}
