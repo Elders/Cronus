02ea2b4c5d9cc5aef20a46a9edbc06b50304d861
mynkow <mynkow@gmail.com>
2014-01-14 19:41:49 +0200

    Bump the version

--- build/_nuget.bat
+++ build/_nuget.bat
@@ -1,2 +1,2 @@
-powershell -ExecutionPolicy Unrestricted -Command "& {Import-Module .\psake.psm1; Invoke-psake .\build.ps1 -properties @{ sln='NMSD.Cronus.sln';config='Release';nugetPackageName='Cronus';nugetSource='NMSD.Cronus';assemblyInformationalVersion='Cronus Beta';assemblyVersion='1.0.0.0';assemblyFileVersion='1.0.7200.?';assemblyRevision='17' }%*} "
+powershell -ExecutionPolicy Unrestricted -Command "& {Import-Module .\psake.psm1; Invoke-psake .\build.ps1 -properties @{ sln='NMSD.Cronus.sln';config='Release';nugetPackageName='Cronus';nugetSource='NMSD.Cronus';assemblyInformationalVersion='Cronus Beta';assemblyVersion='1.0.0.0';assemblyFileVersion='1.0.7200.?';assemblyRevision='18' }%*} "
 PAUSE
