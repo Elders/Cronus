9305dcdca56b714ca44445c6f03a690fff8dbd22
mynkow <mynkow@gmail.com>
2014-01-14 18:12:35 +0200

    Add ValueObject<T> interface. Hope it will be usefull, not sure

---
+++ src/NMSD.Cronus/DomainModelling/IValueObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NMSD.Cronus.DomainModelling
+{
+    public interface IValueObject<T> : IEqualityComparer<T>, IEquatable<T>
+    {
+
+    }
+}
