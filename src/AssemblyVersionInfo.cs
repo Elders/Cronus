aa19d129c12210a9a3adb0a3d15972be87ada86f
simeon.dimov <simeon.dimov@mentormate.com>
2014-01-23 18:25:14 +0200

    Bump version 36

--- src/AssemblyVersionInfo.cs
+++ src/AssemblyVersionInfo.cs
@@ -23,11 +23,11 @@ using System.Runtime.InteropServices;
 //		This is the first charachters of GIT revision.
 
 //	The version of the product which is released
-[assembly: AssemblyInformationalVersion(" / 24dadf9")]
+[assembly: AssemblyInformationalVersion(" / d652325")]
 
 //	Describes file version of the assembly. If you increment Major Version 
 //	you probably want to change the AssemblyVersion as well
-[assembly: AssemblyFileVersion("1.0.35.7200")]
+[assembly: AssemblyFileVersion("1.0.36.7200")]
 
 //	Careful! The CLR uses this version when loading assemblies. 
 //	Change this only when you introduce breaking changes.
