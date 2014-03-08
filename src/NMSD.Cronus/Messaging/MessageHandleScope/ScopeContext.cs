37b0aed970589dd00a2b7d5350bec3bd80af1e9d
mynkow <mynkow@gmail.com>
2014-03-08 22:48:28 +0200

    Initialize scope context properly

--- src/NMSD.Cronus/Messaging/MessageHandleScope/ScopeContext.cs
+++ src/NMSD.Cronus/Messaging/MessageHandleScope/ScopeContext.cs
@@ -5,25 +5,30 @@ namespace NMSD.Cronus.Messaging.MessageHandleScope
 {
     public class ScopeContext : IScopeContext
     {
-        Dictionary<string,Dictionary<Type, object>> context = new Dictionary<string,Dictionary<Type, object>>();
+        Dictionary<string, Dictionary<Type, object>> context = new Dictionary<string, Dictionary<Type, object>>();
+
+        public ScopeContext()
+        {
+            context = new Dictionary<string, Dictionary<Type, object>>();
+        }
 
         public void Set<T>(T obj)
         {
             var objType = obj.GetType();
             var objName = objType.Name;
-            Set<T>(objName,objType, obj);
+            Set<T>(objName, objType, obj);
         }
 
         public void Set<T>(string name, T obj)
         {
             var objType = obj.GetType();
-            Set<T>(name,objType, obj);
+            Set<T>(name, objType, obj);
         }
 
         private void Set<T>(string name, Type objType, object obj)
         {
-            if(!context.ContainsKey(name))
-                context.Add(name, new Dictionary<Type,object>());
+            if (!context.ContainsKey(name))
+                context.Add(name, new Dictionary<Type, object>());
 
             context[name].Add(objType, obj);
         }
@@ -37,7 +42,7 @@ namespace NMSD.Cronus.Messaging.MessageHandleScope
         public T Get<T>(string name)
         {
             object obj;
-            Dictionary<Type,object> objects;
+            Dictionary<Type, object> objects;
 
             var objType = typeof(T);
 
