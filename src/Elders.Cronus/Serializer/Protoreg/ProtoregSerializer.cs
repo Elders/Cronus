632b20adfd222a60cd17a2d0cd39e4602325ec81
mynkow <mynkow@gmail.com>
2014-09-10 02:52:48 +0300

    Add domain modeling package and remove the project reference

--- src/Elders.Cronus/Serializer/Protoreg/ProtoregSerializer.cs
+++ src/Elders.Cronus/Serializer/Protoreg/ProtoregSerializer.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Elders.Cronus.Serializer.Protoreg
+﻿namespace Elders.Cronus.Serializer.Protoreg
 {
     public class ProtoregSerializer : ISerializer
     {
