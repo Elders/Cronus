288b016a8c449243d9f6cc177f15953b0ac64187
Sinstraliz <blagovest.vp@gmail.com>
2014-11-14 15:20:06 +0200

    Added IAggregateVersionService interface.

---
+++ src/Elders.Cronus/EventSourcing/IAggregateVersionService.cs
@@ -0,0 +1,10 @@
+﻿using Elders.Cronus.DomainModeling;
+using System;
+
+namespace Elders.Cronus.EventSourcing
+{
+    public interface IAggregateVersionService : IDisposable
+    {
+        int ReserveVersion(IAggregateRootId aggregateId, int requestedVersion);
+    }
+}
