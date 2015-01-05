8803bb76963715d26ce16f34f020383e02c64415
mynkow <mynkow@gmail.com>
2015-01-05 20:10:59 +0200

    Externalize the serialization intto a separate nuget package

---
+++ src/Elders.Cronus/Userfull/SerializableException.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Elders.Cronus
+{
+    [DataContract(Name = "c3ca519f-5ee8-460e-8f7b-c8a84d2fd191")]
+    public class SerializableException : Exception
+    {
+        SerializableException() { }
+
+        public SerializableException(Exception ex)
+        {
+            ExType = ex.GetType().FullName;
+            ExMessage = ex.Message;
+            ExStackTrace = ex.StackTrace;
+            //ExTargetSite = ex.TargetSite.ToString();
+            ExSource = ex.Source;
+            ExHelpLink = ex.HelpLink;
+
+            if (ex.InnerException != null)
+                ExInnerException = new SerializableException(ex.InnerException);
+        }
+
+        [DataMember(Order = 1)]
+        public string ExType { get; private set; }
+
+        [DataMember(Order = 2)]
+        public string ExMessage { get; private set; }
+
+        [DataMember(Order = 3)]
+        public string ExStackTrace { get; private set; }
+        //public string ExTargetSite { get; private set; }
+
+        [DataMember(Order = 4)]
+        public string ExSource { get; private set; }
+
+        [DataMember(Order = 5)]
+        public string ExHelpLink { get; private set; }
+
+        [DataMember(Order = 100)]
+        public SerializableException ExInnerException { get; private set; }
+    }
+}
