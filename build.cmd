a6707b31b2ec1223f52897a3f2a8c0e5fea52d22
mynkow <mynkow@gmail.com>
2015-06-03 00:01:56 +0300

    Remove the submodule and use Nyx as build script

--- build.cmd
+++ build.cmd
@@ -1,9 +1,42 @@
 @echo off
 
-".\tools\NuGet\NuGet.exe" "install" "FAKE" "-OutputDirectory" ".\bin\tools" "-ExcludeVersion" "-Prerelease"
+SETLOCAL
+
+SET TOOLS_PATH=.\bin\tools
+SET NUGET=%TOOLS_PATH%\NuGet\NuGet.exe
+SET FAKE=%TOOLS_PATH%\FAKE\tools\Fake.exe
+SET NYX=%TOOLS_PATH%\Nyx\tools\build.fsx
+SET BUILD_TOOLS_PATH="%ProgramFiles(x86)%\MSBuild\12.0\bin\MSBuild.exe"
+
+IF NOT EXIST %BUILD_TOOLS_PATH% (
+  echo In order to build or run this tool you need either Visual Studio 2015 or
+  echo Microsoft Build Tools 2013 tools installed.
+  echo.
+  echo Visit this page to download either:
+  echo.
+  echo http://www.microsoft.com/en-us/download/details.aspx?id=40760
+  echo.
+  goto :eof
+)
+
+echo Downloading latest version of NuGet.exe...
+IF NOT EXIST %TOOLS_PATH%\NuGet md %TOOLS_PATH%\NuGet
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest 'https://www.nuget.org/nuget.exe' -OutFile '%NUGET%'"
+
+echo Downloading latest version of Fake.exe...
+%NUGET% "install" "FAKE" "-OutputDirectory" "%TOOLS_PATH%" "-ExcludeVersion" "-Prerelease"
+
+echo Downloading latest version of Nuget.Core...
+%NUGET% "install" "Nuget.Core" "-OutputDirectory" "%TOOLS_PATH%" "-ExcludeVersion" "-Prerelease"
+
+echo Downloading latest version of Nyx...
+%NUGET% "install" "Nyx" "-OutputDirectory" "%TOOLS_PATH%" "-ExcludeVersion" "-Prerelease"
 
 SET TARGET="Build"
 
 IF NOT [%1]==[] (set TARGET="%1")
 
-".\bin\tools\FAKE\tools\Fake.exe" "build.fsx" "target=%TARGET%"
+SET SUMMARY="Elders.Cronus"
+SET DESCRIPTION="Elders.Cronus"
+
+%FAKE% %NYX% "target=%TARGET%" appName=Elders.Cronus appSummary=%SUMMARY% appDescription=%DESCRIPTION% nugetPackageName=Cronus
---
+++ release-local.cmd
@@ -0,0 +1,3 @@
+@echo off
+
+build.cmd ReleaseLocal
