41d3905022b4e4c0bb17abdbb3e3b21bc26a4a80
mynkow <mynkow@gmail.com>
2015-11-10 17:49:51 +0200

    Moved DefaultAggregateRootAtomicAction to the Cronus.AtocmicAction.Redis project.; Added support for cluster configuration and atomic action. In memory atomic action by default.; IAggregateRootLock and IRevisionStore interfaces moved to the Elders.Cronus.AtomicAction.Redis project.

--- src/Elders.Cronus/Properties/AssemblyInfo.cs
+++ src/Elders.Cronus/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyrightAttribute("Copyright ©  2015")]
 [assembly: AssemblyVersionAttribute("2.4.0.0")]
 [assembly: AssemblyFileVersionAttribute("2.4.0.0")]
-[assembly: AssemblyInformationalVersionAttribute("2.4.0-beta.1+1.Branch.release/2.4.0.Sha.3e4f8834fa8a63812fbfc3121045ffd5f065c6af")]
+[assembly: AssemblyInformationalVersionAttribute("2.4.0-beta.2+8.Branch.release/2.4.0.Sha.f759fbf512133adc838afefe3b7ac74dc8ec4010")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const string Version = "2.4.0.0";
