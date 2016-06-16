87d4d856f391a9eb574050c180ad873b9cac5ccc
sdluxeon <sd.luxeon@gmail.com>
2016-06-16 09:05:51 +0300

    Add tests for breaking execution of middleware

---
+++ src/Elders.Cronus.Tests/Middleware/When_breaking_middleware.cs
@@ -0,0 +1,54 @@
+﻿using Elders.Cronus.Middleware;
+using Machine.Specifications;
+using System.Collections.Generic;
+
+namespace Elders.Cronus.Tests.Middleware
+{
+    [Subject("Elders.Cronus.Middleware")]
+    public class When_breaking_middleware
+    {
+        Establish context = () =>
+        {
+            expectedExecution = new List<ExecutionToken>();
+            executionChain = new TestExecutionChain();
+            var mainToken = executionChain.CreateToken();
+            mainMiddleware = new TestMiddleware(mainToken);
+            expectedExecution.Add(mainToken);
+
+
+            var secondToken = executionChain.CreateToken();
+            expectedExecution.Add(secondToken);
+            mainMiddleware.Use((execution) =>
+            {
+                secondToken.Notify();
+                execution.Break();
+
+            });
+
+            thirdToken = executionChain.CreateToken();
+            var thirdMiddleware = new TestMiddleware(thirdToken);
+            mainMiddleware.Use(thirdMiddleware);
+
+        };
+
+        Because of = () => mainMiddleware.Run(invocationContext);
+
+        It the_execution_chain_should_not_be_empty = () => executionChain.GetTokens().ShouldNotBeEmpty();
+
+        It should_have_the_expected_execution = () => executionChain.ShouldMatch(expectedExecution);
+
+        It should_not_contain_the_third_token = () => executionChain.GetTokens().ShouldNotContain(thirdToken);
+
+
+        static TestMiddleware mainMiddleware;
+
+        static TestExecutionChain executionChain;
+
+        static List<ExecutionToken> expectedExecution;
+
+        static ExecutionToken thirdToken;
+
+        static string invocationContext = "Test context";
+
+    }
+}
