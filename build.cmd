d5fe965d0a634b36c21b3210f141af602561e682
SayliS <kalin.venkov@eldersoss.com>
2018-01-09 12:32:50 +0200

    Adds endpoint per bounded context namespace convention

--- build.cmd
+++ build.cmd
@@ -1,52 +1,5 @@
 @echo off
-
-SETLOCAL
-
-SET NUGET=%LocalAppData%\NuGet\NuGet.exe
-SET FAKE=%LocalAppData%\FAKE\tools\Fake.exe
-SET NYX=%LocalAppData%\Nyx\tools\build_next.fsx
-SET GITVERSION=%LocalAppData%\GitVersion.CommandLine\tools\GitVersion.exe
-SET MSBUILD14_TOOLS_PATH="%ProgramFiles(x86)%\MSBuild\14.0\bin\MSBuild.exe"
-SET MSPEC=%LocalAppData%\Machine.Specifications.Runner.Console\tools\mspec-clr4.exe
-
-IF NOT EXIST %MSBUILD14_TOOLS_PATH% (
-  echo In order to run this tool you need either Visual Studio 2015 or
-  echo Microsoft Build Tools 2015 tools installed.
-  echo.
-  echo Visit this page to download either:
-  echo.
-  echo http://www.visualstudio.com/en-us/downloads/visual-studio-2015-downloads-vs
-  echo.
-)
-
-echo Downloading NuGet.exe...
-IF NOT EXIST %LocalAppData%\NuGet md %LocalAppData%\NuGet
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest 'https://dist.nuget.org/win-x86-commandline/latest/nuget.exe' -OutFile '%NUGET%'"
-
-echo Downloading FAKE...
-IF NOT EXIST %LocalAppData%\FAKE %NUGET% "install" "FAKE" "-OutputDirectory" "%LocalAppData%" "-ExcludeVersion" "-Version" "4.32.0"
-
-echo Downloading GitVersion.CommandLine...
-IF NOT EXIST %LocalAppData%\GitVersion.CommandLine %NUGET% "install" "GitVersion.CommandLine" "-OutputDirectory" "%LocalAppData%" "-ExcludeVersion" "-Version" "3.6.1"
-
-echo Downloading Machine.Specifications.Runner.Console...
-IF NOT EXIST %LocalAppData%\Machine.Specifications.Runner.Console %NUGET% "install" "Machine.Specifications.Runner.Console" "-OutputDirectory" "%LocalAppData%" "-ExcludeVersion"
-
-echo Downloading Nyx...
-%NUGET% "install" "Nyx" "-OutputDirectory" "%LocalAppData%" "-ExcludeVersion" "-PreRelease"
-
-%FAKE% %NYX% "target=clean" -st
-%FAKE% %NYX% "target=RestoreNugetPackages" -st
-
-IF NOT [%1]==[] (set RELEASE_NUGETKEY="%1")
-
-SET SUMMARY="Cronus is a lightweight framework for dispatching and receiving messages between microservices with DDD/CQRS in mind"
-SET DESCRIPTION="Cronus is a lightweight framework for dispatching and receiving messages between microservices with DDD/CQRS in mind"
-
-%FAKE% %NYX% "target=RunTests" appName=Elders.Cronus.Tests appSummary=%SUMMARY% appDescription=%DESCRIPTION% appType=tests
-if errorlevel 1 (
-   echo Tests faild with exit code %errorlevel%
-   exit /b %errorlevel%
-)
-
-%FAKE% %NYX% appName=Elders.Cronus appSummary=%SUMMARY% appDescription=%DESCRIPTION% nugetkey=%RELEASE_NUGETKEY% nugetPackageName=Cronus
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "New-Item -ItemType directory -Path .nyx\ -Force; (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Elders/Nyx/master/.nyx/nyx.cmd','.nyx\nyx.cmd')"
+call .nyx\nyx.cmd
+call .nyx\get-dependencies.cmd
+call build-configuration.cmd %1
--- src/Elders.Cronus.Tests/RELEASE_NOTES.md
+++
@@ -1,2 +0,0 @@
-#### 0.1.0 - 01.01.2017
-* Not releaseed. Used only for test runs.
