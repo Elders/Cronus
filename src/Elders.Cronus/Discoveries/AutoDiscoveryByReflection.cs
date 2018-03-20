614af36d03b4e8938adc0df699f99eb1a2713bc0
mynkow <mynkow@gmail.com>
2018-03-20 11:40:13 +0200

    Starts experimenting with Mono.Cecil

--- src/Elders.Cronus/Discoveries/AutoDiscoveryByReflection.cs
+++ src/Elders.Cronus/Discoveries/AutoDiscoveryByReflection.cs
@@ -6,6 +6,7 @@ using System.Reflection;
 using System.Threading;
 using Elders.Cronus.Logging;
 using Elders.Cronus.Pipeline.Config;
+using Mono.Cecil;
 
 namespace Elders.Cronus.Discoveries
 {
@@ -14,7 +15,7 @@ namespace Elders.Cronus.Discoveries
         static readonly ILog log = LogProvider.GetLogger(typeof(DiscoveryBasedOnExecutingDirAssemblies));
 
         static int shouldLoadAssembliesFromDir = 1;
-        static List<Assembly> availableAssemblies = new List<Assembly>();
+        static List<AssemblyDefinition> availableAssemblies = new List<AssemblyDefinition>();
 
         public DiscoveryBasedOnExecutingDirAssemblies()
         {
@@ -34,12 +35,15 @@ namespace Elders.Cronus.Discoveries
         /// </summary>
         /// <param name="builder">Cronus configuration builder. Contains Container property to .Register<T>() whatever you need</param>
         /// <param name="assemblies">List of assemblies to inspect</param>
-        protected abstract void DiscoverFromAssemblies(ISettingsBuilder builder, IEnumerable<Assembly> assemblies);
+        protected abstract void DiscoverFromAssemblies(ISettingsBuilder builder, IEnumerable<AssemblyDefinition> assemblies);
 
         void InitAssemblies()
         {
             if (1 == Interlocked.Exchange(ref shouldLoadAssembliesFromDir, 0))
             {
+
+                AppDomain.CurrentDomain.ReflectionOnlyAssemblyResolve += new ResolveEventHandler(CurrentDomain_ReflectionOnlyAssemblyResolve);
+
                 string codeBase = Assembly.GetExecutingAssembly().CodeBase;
                 UriBuilder uri = new UriBuilder(codeBase);
                 string path = Uri.UnescapeDataString(uri.Path);
@@ -50,8 +54,8 @@ namespace Elders.Cronus.Discoveries
                 {
                     try
                     {
-                        var reflAssembly = Assembly.ReflectionOnlyLoadFrom(file);
-                        availableAssemblies.Add(reflAssembly);
+                        var assemblyDefinition = Mono.Cecil.AssemblyDefinition.ReadAssembly(file);
+                        availableAssemblies.Add(assemblyDefinition);
                     }
                     catch (Exception ex)
                     {
@@ -60,15 +64,25 @@ namespace Elders.Cronus.Discoveries
                 }
             }
         }
+
+        static Assembly CurrentDomain_ReflectionOnlyAssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            return System.Reflection.Assembly.ReflectionOnlyLoad(args.Name);
+        }
     }
 
     public class FindDiscoveries : DiscoveryBasedOnExecutingDirAssemblies
     {
-        protected override void DiscoverFromAssemblies(ISettingsBuilder builder, IEnumerable<Assembly> assemblies)
+        protected override void DiscoverFromAssemblies(ISettingsBuilder builder, IEnumerable<AssemblyDefinition> assemblies)
         {
-            var discoveries = assemblies.Where(ass => ass.IsDynamic == false)
-                .SelectMany(ass => ass.GetExportedTypes().Where(type => type.IsAbstract == false && type.IsClass && typeof(IDiscovery).IsAssignableFrom(type) && type != typeof(FindDiscoveries)))
-                .Select(dt => (IDiscovery)FastActivator.CreateInstance(dt))
+            var discoveries = assemblies.SelectMany(x => x.Modules)
+                .SelectMany(mod => mod.GetTypes().Where(type =>
+                {
+                    var td = mod.ImportReference(typeof(IDiscovery)).Resolve();
+                    var ignore = mod.ImportReference(typeof(FindDiscoveries)).Resolve();
+                    return type.IsAbstract == false && type.IsClass && td.IsAssignableFrom(type) && type != ignore;
+                }))
+                .Select(dt => (IDiscovery)FastActivator.CreateInstance(dt.ToType()))
                 .ToList();
 
             discoveries.ForEach(x => x.Discover(builder));
@@ -86,6 +100,93 @@ namespace Elders.Cronus.Discoveries
         public static IEnumerable<string> GetFiles(this string path, string[] searchPatterns, SearchOption searchOption = SearchOption.TopDirectoryOnly)
         {
             return searchPatterns.AsParallel().SelectMany(searchPattern => Directory.EnumerateFiles(path, searchPattern, searchOption));
+        }
+    }
+
+    static internal class TypeDefinitionExtensions
+    {
+        /// <summary>
+        /// Is childTypeDef a subclass of parentTypeDef. Does not test interface inheritance
+        /// </summary>
+        /// <param name="childTypeDef"></param>
+        /// <param name="parentTypeDef"></param>
+        /// <returns></returns>
+        public static bool IsSubclassOf(this TypeDefinition childTypeDef, TypeDefinition parentTypeDef)
+        {
+            return
+                childTypeDef.MetadataToken != parentTypeDef.MetadataToken && childTypeDef.EnumerateBaseClasses()
+                .Any(b => b.MetadataToken == parentTypeDef.MetadataToken);
+        }
+
+        /// <summary>
+        /// Does childType inherit from parentInterface
+        /// </summary>
+        /// <param name="childType"></param>
+        /// <param name="parentInterfaceDef"></param>
+        /// <returns></returns>
+        public static bool DoesAnySubTypeImplementInterface(this TypeDefinition childType, TypeDefinition parentInterfaceDef)
+        {
+            return childType
+               .EnumerateBaseClasses()
+               .Any(typeDefinition => typeDefinition.DoesSpecificTypeImplementInterface(parentInterfaceDef));
+        }
+
+        /// <summary>
+        /// Does the childType directly inherit from parentInterface. Base
+        /// classes of childType are not tested
+        /// </summary>
+        /// <param name="childTypeDef"></param>
+        /// <param name="parentInterfaceDef"></param>
+        /// <returns></returns>
+        public static bool DoesSpecificTypeImplementInterface(this TypeDefinition childTypeDef, TypeDefinition parentInterfaceDef)
+        {
+            return childTypeDef
+               .Interfaces
+               .Any(ifaceDef => DoesSpecificInterfaceImplementInterface(ifaceDef.InterfaceType.Resolve(), parentInterfaceDef));
+        }
+
+        /// <summary>
+        /// Does interface iface0 equal or implement interface iface1
+        /// </summary>
+        /// <param name="iface0"></param>
+        /// <param name="iface1"></param>
+        /// <returns></returns>
+        public static bool DoesSpecificInterfaceImplementInterface(TypeDefinition iface0, TypeDefinition iface1)
+        {
+            return iface0.MetadataToken == iface1.MetadataToken || iface0.DoesAnySubTypeImplementInterface(iface1);
+        }
+
+        /// <summary>
+        /// Is source type assignable to target type
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static bool IsAssignableFrom(this TypeDefinition target, TypeDefinition source)
+        {
+            return
+                target == source ||
+                target.MetadataToken == source.MetadataToken ||
+                source.IsSubclassOf(target) ||
+                target.IsInterface && source.DoesAnySubTypeImplementInterface(target);
+        }
+
+        /// <summary>
+        /// Enumerate the current type, it's parent and all the way to the top type
+        /// </summary>
+        /// <param name="klassType"></param>
+        /// <returns></returns>
+        public static IEnumerable<TypeDefinition> EnumerateBaseClasses(this TypeDefinition klassType)
+        {
+            for (var typeDefinition = klassType; typeDefinition != null; typeDefinition = typeDefinition.BaseType?.Resolve())
+            {
+                yield return typeDefinition;
+            }
+        }
+
+        public static Type ToType(this TypeDefinition typeDef)
+        {
+            return Type.GetType(typeDef.FullName + ", " + typeDef.Module.Assembly.FullName);
         }
     }
 }
