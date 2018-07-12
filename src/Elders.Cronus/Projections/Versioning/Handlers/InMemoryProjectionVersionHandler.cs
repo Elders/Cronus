527f4eb9624c509b0b6f556b576240833629e8b3
mynkow <mynkow@gmail.com>
2018-07-12 21:22:56 +0300

    Code formatting

--- src/Elders.Cronus/Projections/Versioning/Handlers/InMemoryProjectionVersionHandler.cs
+++ src/Elders.Cronus/Projections/Versioning/Handlers/InMemoryProjectionVersionHandler.cs
@@ -4,9 +4,9 @@ namespace Elders.Cronus.Projections.Versioning
 {
     [DataContract(Name = "f92c320f-ef20-49aa-a8bc-c7085cc3a731")]
     public class InMemoryProjectionVersionHandler : ISystemProjection,
-            IEventHandler<ProjectionVersionRequested>,
-            IEventHandler<NewProjectionVersionIsNowLive>,
-            IEventHandler<ProjectionVersionRequestCanceled>
+        IEventHandler<ProjectionVersionRequested>,
+        IEventHandler<NewProjectionVersionIsNowLive>,
+        IEventHandler<ProjectionVersionRequestCanceled>
     {
         public InMemoryProjectionVersionStore ProjectionVersionStore { get; set; }
 
