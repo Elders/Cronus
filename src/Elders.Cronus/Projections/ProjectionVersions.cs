dadc63a63bcddaa24715ed9109523ff2b5199247
mynkow <mynkow@gmail.com>
2018-08-15 10:00:26 +0300

    Code cleanup

--- src/Elders.Cronus/Projections/ProjectionVersions.cs
+++ src/Elders.Cronus/Projections/ProjectionVersions.cs
@@ -1,8 +1,8 @@
-﻿using System.Runtime.Serialization;
-using System;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Collections;
+using System.Runtime.Serialization;
 
 namespace Elders.Cronus.Projections
 {
