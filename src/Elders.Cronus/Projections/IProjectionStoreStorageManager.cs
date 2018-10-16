6c2d5fd941822590685f9192fe92bf256c9252e1
tokmac <lyubomir.mitkov@gmail.com>
2018-10-16 14:56:17 +0300

    Added IProjectionStoreStorageManager interface

---
+++ src/Elders.Cronus/Projections/IProjectionStoreStorageManager.cs
@@ -0,0 +1,12 @@
+﻿namespace Elders.Cronus.Projections
+{
+    public interface IProjectionStoreStorageManager
+    {
+        void CreateProjectionsStorage();
+
+        /// <summary>
+        /// Creates the database for the projection store including the Projections and Snapshots tables. If any exists it is not overwritten.
+        /// </summary>
+        void CreateStorage();
+    }
+}
