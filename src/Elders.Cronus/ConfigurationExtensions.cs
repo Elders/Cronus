0818ce5e384bd8facaeb006a2bb1e9007698b7c8
mynkow <mynkow@gmail.com>
2018-10-26 14:37:55 +0300

    Adds IConfiguration extensions

---
+++ src/Elders.Cronus/ConfigurationExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace Elders.Cronus
+{
+    public static class ConfigurationExtensions
+    {
+        public static string GetRequired(this IConfiguration configuration, string key)
+        {
+            string value = configuration[key];
+            if (string.IsNullOrEmpty(value)) throw new ArgumentException($"Missing setting: {key}");
+
+            return value;
+        }
+    }
+}
