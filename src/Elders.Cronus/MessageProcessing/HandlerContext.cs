35b7bc4fd4458f55fe71ff84988eb09237c2f779
mynkow <mynkow@gmail.com>
2018-11-12 23:22:59 +0200

    Moves CronusContext to a separate file

--- src/Elders.Cronus/MessageProcessing/HandlerContext.cs
+++ src/Elders.Cronus/MessageProcessing/HandlerContext.cs
@@ -21,33 +21,4 @@ namespace Elders.Cronus.MessageProcessing
             return $"{HandlerInstance.GetType().Name}({Message.GetType().Name})";
         }
     }
-
-    public sealed class CronusContext
-    {
-        private string tenant;
-
-        public string Tenant
-        {
-            get
-            {
-                if (string.IsNullOrEmpty(tenant)) throw new ArgumentException("Unknown tenant. CronusContext is not properly built. Make sure that someone properly resolves the current tenant and sets it to this instance.");
-                return tenant;
-            }
-            private set
-            {
-                if (string.IsNullOrEmpty(value)) throw new ArgumentNullException(nameof(Tenant));
-                tenant = value;
-            }
-        }
-
-        public bool IsNotInitialized => string.IsNullOrEmpty(tenant) || ServiceProvider is null;
-
-        public IServiceProvider ServiceProvider { get; private set; }
-
-        public void Initialize(string tenant, IServiceProvider serviceProvider)
-        {
-            Tenant = tenant;
-            ServiceProvider = serviceProvider;
-        }
-    }
 }
