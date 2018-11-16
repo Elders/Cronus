d2073352588085596dac577e146001c94fbe4e39
mynkow <mynkow@gmail.com>
2018-11-16 20:53:36 +0200

    Adds more context to the AggregateCommitRaw

--- src/Elders.Cronus/EventStore/AggregateCommitRaw.cs
+++ src/Elders.Cronus/EventStore/AggregateCommitRaw.cs
@@ -7,14 +7,20 @@
     /// </summary>
     public sealed class AggregateCommitRaw
     {
-        public AggregateCommitRaw(string aggregateRootId, byte[] data)
+        public AggregateCommitRaw(string aggregateRootId, byte[] data, int revision, long timestamp)
         {
             AggregateRootId = aggregateRootId;
             Data = data;
+            Revision = revision;
+            Timestamp = timestamp;
         }
 
         public string AggregateRootId { get; private set; }
 
         public byte[] Data { get; private set; }
+
+        public int Revision { get; private set; }
+
+        public long Timestamp { get; private set; }
     }
 }
