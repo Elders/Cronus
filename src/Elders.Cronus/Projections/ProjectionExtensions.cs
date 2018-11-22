6716b530911287c6fbe82b145cf377904db135f4
AlexPlom <alexiosplomaritis@gmail.com>
2018-11-22 21:46:32 +0200

    Removes the extension which calculates hash

--- src/Elders.Cronus/Projections/ProjectionExtensions.cs
+++ src/Elders.Cronus/Projections/ProjectionExtensions.cs
@@ -4,12 +4,6 @@ namespace Elders.Cronus.Projections
 {
     public static class ProjectionExtensions
     {
-        static Versioning.ProjectionHasher hasher = new Versioning.ProjectionHasher();
-        public static string GetProjectionHash(this Type projectionType)
-        {
-            return hasher.CalculateHash(projectionType);
-        }
-
         public static bool IsSnapshotable(this Type projectionType)
         {
             return typeof(Snapshotting.IAmNotSnapshotable).IsAssignableFrom(projectionType) == false;
