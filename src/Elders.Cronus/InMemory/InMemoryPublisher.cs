aa91087cecac6fafba0ef2730b319f43a7684c12
tokmac <lyubomir.mitkov@gmail.com>
2018-12-04 14:18:12 +0200

    fix after merge

--- src/Elders.Cronus/InMemory/InMemoryPublisher.cs
+++ src/Elders.Cronus/InMemory/InMemoryPublisher.cs
@@ -8,16 +8,11 @@ namespace Elders.Cronus.InMemory
     {
         static readonly ILog log = LogProvider.GetLogger(typeof(InMemoryPublisher<>));
 
-<<<<<<< Updated upstream
-
-        public InMemoryPublisher(SubscriberCollection<object> messageProcessor)
-=======
         private readonly SubscriberCollection<IApplicationService> subscribtions;
         private readonly InMemoryQueue messageQueue;
 
         public InMemoryPublisher(SubscriberCollection<IApplicationService> messageProcessor, InMemoryQueue messageQueue)
             : base(new DefaultTenantResolver())
->>>>>>> Stashed changes
         {
             this.subscribtions = messageProcessor;
             this.messageQueue = messageQueue;
