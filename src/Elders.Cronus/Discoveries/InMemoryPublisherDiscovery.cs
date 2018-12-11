1d8178bf1c420165a0888cdcae4965585c525c36
mynkow <mynkow@gmail.com>
2018-12-11 22:42:53 +0200

    Introduces InMemoryDiscovery which adds default in memory services

--- src/Elders.Cronus/Discoveries/InMemoryPublisherDiscovery.cs
+++ src/Elders.Cronus/Discoveries/InMemoryPublisherDiscovery.cs
@@ -1,24 +1,22 @@
-﻿//using System.Collections.Generic;
-//using Elders.Cronus.AtomicAction;
-//using Elders.Cronus.AtomicAction.InMemory;
-//using Elders.Cronus.InMemory;
-//using Microsoft.Extensions.DependencyInjection;
+﻿using System.Collections.Generic;
+using Elders.Cronus.AtomicAction;
+using Elders.Cronus.AtomicAction.InMemory;
+using Elders.Cronus.InMemory;
+using Microsoft.Extensions.DependencyInjection;
 
-//namespace Elders.Cronus.Discoveries
-//{
-//    public class InMemoryDiscovery : DiscoveryBasedOnExecutingDirAssemblies<IPublisher<IMessage>>
-//    {
-//        protected override DiscoveryResult<IPublisher<IMessage>> DiscoverFromAssemblies(DiscoveryContext context)
-//        {
-//            return new DiscoveryResult<IPublisher<IMessage>>(GetModels());
-//        }
+namespace Elders.Cronus.Discoveries
+{
+    public class InMemoryDiscovery : DiscoveryBasedOnExecutingDirAssemblies<IPublisher<IMessage>>
+    {
+        protected override DiscoveryResult<IPublisher<IMessage>> DiscoverFromAssemblies(DiscoveryContext context)
+        {
+            return new DiscoveryResult<IPublisher<IMessage>>(GetModels());
+        }
 
-//        IEnumerable<DiscoveredModel> GetModels()
-//        {
-//            yield return new DiscoveredModel(typeof(InMemoryQueue), typeof(InMemoryQueue), ServiceLifetime.Singleton);
-//            yield return new DiscoveredModel(typeof(IPublisher<>), typeof(SynchronousMessageProcessor<>), ServiceLifetime.Singleton);
-//            yield return new DiscoveredModel(typeof(IConsumer<>), typeof(InMemoryConsumer<>), ServiceLifetime.Transient);
-//            yield return new DiscoveredModel(typeof(IAggregateRootAtomicAction), typeof(InMemoryAggregateRootAtomicAction), ServiceLifetime.Transient);
-//        }
-//    }
-//}
+        IEnumerable<DiscoveredModel> GetModels()
+        {
+            yield return new DiscoveredModel(typeof(IPublisher<>), typeof(SynchronousMessageProcessor<>), ServiceLifetime.Singleton);
+            yield return new DiscoveredModel(typeof(IAggregateRootAtomicAction), typeof(InMemoryAggregateRootAtomicAction), ServiceLifetime.Transient);
+        }
+    }
+}
