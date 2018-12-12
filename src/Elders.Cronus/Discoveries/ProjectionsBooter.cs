4abc226c6d8f80b9496c1f69e5599d513ecccf69
mynkow <mynkow@gmail.com>
2018-12-12 15:26:55 +0200

    Registers all ICronusStarup services

--- src/Elders.Cronus/Discoveries/ProjectionsBooter.cs
+++ src/Elders.Cronus/Discoveries/ProjectionsBooter.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using Elders.Cronus.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Elders.Cronus.Discoveries
 {
-    public class ProjectionsBooter : DiscoveryBase<ICronusHost>
+    public class CronusHostDiscovery : DiscoveryBase<ICronusHost>
     {
         protected override DiscoveryResult<ICronusHost> DiscoverFromAssemblies(DiscoveryContext context)
         {
@@ -13,15 +16,32 @@ namespace Elders.Cronus.Discoveries
 
         IEnumerable<DiscoveredModel> GetModels(DiscoveryContext context)
         {
+            foreach (var startupType in context.Assemblies.Find<ICronusStartup>())
+            {
+                yield return new DiscoveredModel(startupType, startupType, ServiceLifetime.Singleton);
+            }
+
+
             yield return new DiscoveredModel(typeof(ICronusHost), typeof(CronusHost), ServiceLifetime.Transient);
 
             yield return new DiscoveredModel(typeof(Cronus.ProjectionsBooter), typeof(Cronus.ProjectionsBooter), ServiceLifetime.Transient);
 
             yield return new DiscoveredModel(typeof(BoundedContext), typeof(BoundedContext), ServiceLifetime.Transient);
 
-            var loadedTypes = context.Assemblies.SelectMany(asm => asm.GetLoadableTypes())
-                .Where(type => type.IsAbstract == false && type.IsInterface == false && typeof(IEvent).IsAssignableFrom(type) && type != typeof(EntityEvent));
+            var loadedTypes = context.Assemblies.Find<IEvent>().Where(type => type != typeof(EntityEvent));
             yield return new DiscoveredModel(typeof(TypeContainer<IEvent>), new TypeContainer<IEvent>(loadedTypes));
+        }
+    }
+
+    public static class DiscoveryAssemblyExtensions
+    {
+        public static IEnumerable<Type> Find<TService>(this IEnumerable<Assembly> assemblies)
+        {
+            return assemblies
+                .SelectMany(asm => asm.GetLoadableTypes())
+                .Where(type => type.IsAbstract == false)
+                .Where(type => type.IsInterface == false)
+                .Where(type => typeof(TService).IsAssignableFrom(type));
         }
     }
 }
