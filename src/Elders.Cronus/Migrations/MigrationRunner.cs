d52a1ee50530be9937ba1be4e3496dbb05e5a2c4
mynkow <mynkow@gmail.com>
2018-12-23 13:14:19 +0200

    Moves CopyEventStore to a separate file

--- src/Elders.Cronus/Migrations/MigrationRunner.cs
+++ src/Elders.Cronus/Migrations/MigrationRunner.cs
@@ -24,26 +24,4 @@ namespace Elders.Cronus.Migrations
         public abstract void Run(IEnumerable<IMigration<TDataFormat>> migrations);
 
     }
-
-    public class CopyEventStore<TSourceEventStorePlayer, TTargetEventStore> : MigrationRunnerBase<AggregateCommitRaw, TSourceEventStorePlayer, TTargetEventStore>
-        where TSourceEventStorePlayer : IEventStorePlayer
-        where TTargetEventStore : IEventStore
-    {
-        private static readonly ILog log = LogProvider.GetLogger(typeof(MigrationRunnerBase<,,>));
-
-        public CopyEventStore(TSourceEventStorePlayer source, TTargetEventStore target) : base(source, target) { }
-
-        public override void Run(IEnumerable<IMigration<AggregateCommitRaw>> migrations)
-        {
-            int counter = 0;
-            foreach (var sourceCommit in source.LoadAggregateCommitsRaw(5000))
-            {
-                if (counter % 10000 == 0) log.Info($"[Migrations] Migrated records: {counter}");
-
-                target.Append(sourceCommit);
-
-                counter++;
-            }
-        }
-    }
 }
