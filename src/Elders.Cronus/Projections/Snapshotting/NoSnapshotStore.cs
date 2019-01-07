2375f9606e50f54077fd46df3a40e846675a93bd
mynkow <mynkow@gmail.com>
2019-01-07 11:10:24 +0200

    Adds async load methods for projections ISnapshotStore

--- src/Elders.Cronus/Projections/Snapshotting/NoSnapshotStore.cs
+++ src/Elders.Cronus/Projections/Snapshotting/NoSnapshotStore.cs
@@ -1,4 +1,6 @@
-﻿namespace Elders.Cronus.Projections.Snapshotting
+﻿using System.Threading.Tasks;
+
+namespace Elders.Cronus.Projections.Snapshotting
 {
     public class NoSnapshotStore : ISnapshotStore
     {
@@ -9,9 +11,19 @@
             return new NoSnapshot(id, projectionName);
         }
 
+        public Task<ISnapshot> LoadAsync(string projectionName, IBlobId id, ProjectionVersion version)
+        {
+            return Task.FromResult(Load(projectionName, id, version));
+        }
+
         public SnapshotMeta LoadMeta(string projectionName, IBlobId id, ProjectionVersion version)
         {
             return new NoSnapshot(id, projectionName).GetMeta();
+        }
+
+        public Task<SnapshotMeta> LoadMetaAsync(string projectionName, IBlobId id, ProjectionVersion version)
+        {
+            return Task.FromResult(LoadMeta(projectionName, id, version));
         }
 
         public void Save(ISnapshot snapshot, ProjectionVersion version) { }
