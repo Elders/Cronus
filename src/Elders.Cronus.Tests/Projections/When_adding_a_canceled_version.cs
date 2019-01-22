f462b54fba9716e79427c02544a0fe2ffc74f9c1
mynkow <mynkow@gmail.com>
2019-01-22 13:58:05 +0200

    Splits classes onto separate files

---
+++ src/Elders.Cronus.Tests/Projections/When_adding_a_canceled_version.cs
@@ -0,0 +1,28 @@
+﻿using Machine.Specifications;
+
+namespace Elders.Cronus.Projections
+{
+    [Subject("ProjectionVersions")]
+    public class When_adding_a_canceled_version
+    {
+        Establish context = () =>
+        {
+            version = new ProjectionVersion("projectionName", ProjectionStatus.Canceled, 1, "hash");
+            nextVersion = new ProjectionVersion("projectionName", ProjectionStatus.Building, 2, "hash");
+            versions = new ProjectionVersions();
+        };
+
+        Because of = () => versions.Add(version);
+
+        It should_have_next_version = () => versions.GetNext().ShouldEqual(nextVersion);
+        It should_not_have_live_version = () => versions.GetLive().ShouldBeNull();
+
+        It should_not_be__canceled__ = () => versions.IsCanceled(version).ShouldBeTrue();
+        It should_not_be__outdated__ = () => versions.IsOutdatad(version).ShouldBeFalse();
+        It should_be__not_present__ = () => versions.IsNotPresent().ShouldBeFalse();
+
+        static ProjectionVersion version;
+        static ProjectionVersion nextVersion;
+        static ProjectionVersions versions;
+    }
+}
