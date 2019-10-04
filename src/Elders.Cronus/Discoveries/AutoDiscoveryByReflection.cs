18bed00155dec4d6a2f5775de731b8ffc4e34f09
mynkow <mynkow@gmail.com>
2019-10-04 18:17:06 +0300

    Removes obsolete method

--- src/Elders.Cronus/Discoveries/AutoDiscoveryByReflection.cs
+++ src/Elders.Cronus/Discoveries/AutoDiscoveryByReflection.cs
@@ -9,27 +9,6 @@ namespace Elders.Cronus.Discoveries
         IConfiguration Configuration { get; set; }
     }
 
-    [Obsolete("Use DiscoveryBase instead. Will be removed in v6.0.0")]
-    public abstract class DiscoveryBasedOnExecutingDirAssemblies<TCronusService> : IDiscovery<TCronusService>, IHaveConfiguration
-    //where TCronusService : ICronusService
-    {
-        public virtual string Name { get { return this.GetType().Name; } }
-
-        public IConfiguration Configuration { get; set; }
-        public IServiceCollection Services { get; set; }
-
-        public IDiscoveryResult<TCronusService> Discover()
-        {
-            DiscoveryContext context = new DiscoveryContext();
-            context.Configuration = Configuration;
-            context.Assemblies = AssemblyLoader.Assemblies.Values;
-            var discoveryResult = DiscoverFromAssemblies(context);
-            return discoveryResult;
-        }
-
-        protected abstract DiscoveryResult<TCronusService> DiscoverFromAssemblies(DiscoveryContext context);
-    }
-
     public abstract class DiscoveryBase<TCronusService> : IDiscovery<TCronusService>, IHaveConfiguration
     //where TCronusService : ICronusService
     {
