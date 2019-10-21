b47262ab12d71105a6d7ff97e0c4241f92b07e59
mynkow <mynkow@gmail.com>
2019-10-21 18:50:21 +0300

    Organizes EventStore index files

--- src/Elders.Cronus/EventStore/Index/IndexRecord.cs
+++ src/Elders.Cronus/EventStore/Index/IndexRecord.cs
@@ -1,19 +1,5 @@
-﻿using System.Collections.Generic;
-
-namespace Elders.Cronus.EventStore.Index
+﻿namespace Elders.Cronus.EventStore.Index
 {
-    public interface IIndexStatusStore
-    {
-        void Save(string indexId, IndexStatus status);
-        IndexStatus Get(string indexId);
-    }
-
-    public interface IIndexStore
-    {
-        void Apend(IEnumerable<IndexRecord> indexRecords);
-        IEnumerable<IndexRecord> Get(string indexRecordId);
-    }
-
     public class IndexRecord
     {
         public IndexRecord(string id, byte[] aggregateRootId)
