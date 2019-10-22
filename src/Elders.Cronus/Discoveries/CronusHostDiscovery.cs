8db9dc98bd4cc529630904f4dd55ce3cb103b176
mynkow <mynkow@gmail.com>
2019-10-22 22:49:16 +0300

    Organizes the types CronusHostDiscovery and DiscoveryAssemblyExtensions

--- src/Elders.Cronus/Discoveries/CronusHostDiscovery.cs
+++ src/Elders.Cronus/Discoveries/CronusHostDiscovery.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Elders.Cronus.Discoveries
@@ -20,27 +18,14 @@ namespace Elders.Cronus.Discoveries
                 yield return new DiscoveredModel(startupType, startupType, ServiceLifetime.Singleton);
             }
 
-
             yield return new DiscoveredModel(typeof(ICronusHost), typeof(CronusHost), ServiceLifetime.Transient);
 
-            yield return new DiscoveredModel(typeof(Cronus.ProjectionsBooter), typeof(Cronus.ProjectionsBooter), ServiceLifetime.Transient);
+            yield return new DiscoveredModel(typeof(Cronus.ProjectionsStartup), typeof(Cronus.ProjectionsStartup), ServiceLifetime.Transient);
 
             yield return new DiscoveredModel(typeof(BoundedContext), typeof(BoundedContext), ServiceLifetime.Transient);
 
             var loadedTypes = context.Assemblies.Find<IEvent>().Where(type => type != typeof(EntityEvent));
             yield return new DiscoveredModel(typeof(TypeContainer<IEvent>), new TypeContainer<IEvent>(loadedTypes));
-        }
-    }
-
-    public static class DiscoveryAssemblyExtensions
-    {
-        public static IEnumerable<Type> Find<TService>(this IEnumerable<Assembly> assemblies)
-        {
-            return assemblies
-                .SelectMany(asm => asm.GetLoadableTypes())
-                .Where(type => type.IsAbstract == false)
-                .Where(type => type.IsInterface == false)
-                .Where(type => typeof(TService).IsAssignableFrom(type));
         }
     }
 }
