2e393ee6c90e0dd992ab117a76797e98d4f1a67d
mynkow <mynkow@gmail.com>
2019-10-30 09:56:47 +0200

    Organizes Tests

---
+++ src/Elders.Cronus.Tests/Discoveries/Models.cs
@@ -0,0 +1,26 @@
+﻿using Elders.Cronus.Discoveries;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+
+namespace Elders.Cronus.Tests.InMemoryEventStoreSuite
+{
+    public interface ITestService { }
+    public interface DefaultService : ITestService { }
+    public interface OverriderService : ITestService { }
+
+    public class OverriderDiscoveredModel : IDiscoveryResult<ITestService>
+    {
+        public IEnumerable<DiscoveredModel> Models => new List<DiscoveredModel>()
+        {
+            new DiscoveredModel(typeof(ITestService), typeof(OverriderService), ServiceLifetime.Transient)
+        };
+    }
+
+    public class CronusServicesProviderTest : CronusServicesProvider
+    {
+        public CronusServicesProviderTest(IServiceCollection services, IConfiguration configuration) : base(services, configuration) { }
+
+        protected virtual void Handle(DiscoveryResult<ITestService> discoveryResult) => AddServices(discoveryResult);
+    }
+}
