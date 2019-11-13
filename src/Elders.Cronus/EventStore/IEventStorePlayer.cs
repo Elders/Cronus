d7835dee8054b6c09d676e45ae5c7670e663bfb0
mynkow <mynkow@gmail.com>
2019-11-13 13:44:51 +0200

    Initializes the commits collection

--- src/Elders.Cronus/EventStore/IEventStorePlayer.cs
+++ src/Elders.Cronus/EventStore/IEventStorePlayer.cs
@@ -32,6 +32,11 @@ namespace Elders.Cronus.EventStore
 
     public class LoadAggregateCommitsResult
     {
+        public LoadAggregateCommitsResult()
+        {
+            Commits = new List<AggregateCommit>();
+        }
+
         public string PaginationToken { get; set; }
 
         public List<AggregateCommit> Commits { get; set; }
