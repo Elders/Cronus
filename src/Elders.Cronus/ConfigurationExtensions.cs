880ce85b77a8ac66596573d21c236eda3a97644b
tokmac <lyubomir.mitkov@gmail.com>
2019-11-15 16:45:20 +0200

    Added in ConfigurationExtensions - GetRequired for IConfigurationSection and GetOptional

--- src/Elders.Cronus/ConfigurationExtensions.cs
+++ src/Elders.Cronus/ConfigurationExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.Extensions.Configuration;
 
 namespace Elders.Cronus
@@ -11,6 +12,24 @@ namespace Elders.Cronus
             if (string.IsNullOrEmpty(value)) throw new ArgumentException($"Missing setting: {key} (for reference see here  https://github.com/Elders/Cronus/blob/master/doc/Configuration.md)");
 
             return value;
+        }
+
+        public static string GetRequired(this IConfigurationSection configuration, string key)
+        {
+            string value = configuration.GetChildren().FirstOrDefault(item => item.Key == key)?.Value;
+            if (string.IsNullOrEmpty(value)) throw new ArgumentException($"Missing setting: {configuration.Key}:{key} (for reference see here  https://github.com/Elders/Cronus/blob/master/doc/Configuration.md)");
+
+            return value;
+        }
+
+        public static string GetOptional(this IConfiguration configuration, string key, string defaultValue)
+        {
+            string value = configuration[key];
+
+            if (string.IsNullOrEmpty(value) == false) return value;
+            if (string.IsNullOrEmpty(defaultValue)) throw new ArgumentException($"Missing setting: {key} (for reference see here  https://github.com/Elders/Cronus/blob/master/doc/Configuration.md)");
+
+            return defaultValue;
         }
     }
 }
