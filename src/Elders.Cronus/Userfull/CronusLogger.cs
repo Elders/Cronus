bb702dc2f16dafc97a7f0187be33691d0848eed3
Konstantin Tenekedzhiev <konstantin.tenekedzhiev@eldersoss.com>
2020-04-11 23:07:52 +0300

    CronusLogger

---
+++ src/Elders.Cronus/Userfull/CronusLogger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace Elders.Cronus
+{
+    internal static class CronusLogger
+    {
+        private static ILoggerFactory factory = new LoggerFactory();
+
+        public static void SetLoggerFactory(ILoggerFactory factory)
+        {
+            if (factory is null) throw new ArgumentNullException(nameof(factory));
+
+            CronusLogger.factory = factory;
+        }
+
+        public static ILogger<T> CreateLogger<T>() => factory.CreateLogger<T>();
+    }
+}
