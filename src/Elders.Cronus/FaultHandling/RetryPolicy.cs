08dd0788e37a3f62ca8f0cfbc4b82bc2b09c9863
Konstantin Tenekedzhiev <konstantin.tenekedzhiev@eldersoss.com>
2020-04-13 20:16:21 +0300

    Pass through format parameters

--- src/Elders.Cronus/FaultHandling/RetryPolicy.cs
+++ src/Elders.Cronus/FaultHandling/RetryPolicy.cs
@@ -200,8 +200,7 @@ namespace Elders.Cronus.FaultHandling
         {
             if (this.Retrying != null)
             {
-                logger.Info(() => string.Format("Retrying - Count: {0}, Delay: {1}, Exception: {2}",
-                                                          retryCount, delay.TotalMilliseconds, lastError.Message));
+                logger.Info("Retrying - Count: {0}, Delay: {1}, Exception: {2}", retryCount, delay.TotalMilliseconds, lastError.Message);
 
                 this.Retrying(this, new RetryingEventArgs(retryCount, delay, lastError));
             }
