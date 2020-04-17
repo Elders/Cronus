f2af8c5346837abae8cd7e8c8492cd70994d8675
Konstantin Tenekedzhiev <konstantin.tenekedzhiev@eldersoss.com>
2020-04-17 21:06:01 +0300

    Basic in-memory implementations

---
+++ src/Elders.Cronus/EventStore/InMemory/InMemoryIndexStore.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using Elders.Cronus.EventStore.Index;
+
+namespace Elders.Cronus.EventStore.InMemory
+{
+    public class InMemoryIndexStore : IIndexStore
+    {
+        private ConcurrentDictionary<string, List<IndexRecord>> indexRecords = new ConcurrentDictionary<string, List<IndexRecord>>();
+
+        public void Apend(IEnumerable<IndexRecord> indexRecords)
+        {
+            foreach (var record in indexRecords)
+            {
+                if (this.indexRecords.ContainsKey(record.Id) == false)
+                    this.indexRecords.TryAdd(record.Id, new List<IndexRecord>());
+
+                this.indexRecords[record.Id].Add(record);
+            }
+        }
+
+        public IEnumerable<IndexRecord> Get(string indexRecordId)
+        {
+            if (indexRecords.ContainsKey(indexRecordId))
+                return indexRecords[indexRecordId];
+
+            return Enumerable.Empty<IndexRecord>();
+        }
+    }
+}
---
+++ src/Elders.Cronus/Projections/InMemory/InMemorySnapshotStore.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Elders.Cronus.Projections.Snapshotting;
+
+namespace Elders.Cronus.Projections.InMemory
+{
+    public class InMemorySnapshotStore : ISnapshotStore
+    {
+        private readonly ConcurrentDictionary<ProjectionVersion, List<ISnapshot>> snapshots = new ConcurrentDictionary<ProjectionVersion, List<ISnapshot>>();
+
+        public ISnapshot Load(string projectionName, IBlobId id, ProjectionVersion version)
+        {
+            if (snapshots.ContainsKey(version) == false)
+                return new NoSnapshot(id, projectionName);
+
+            var snapshot = snapshots[version].FirstOrDefault(x => x.ProjectionName == projectionName && x.Id == id);
+            return snapshot ?? new NoSnapshot(id, projectionName);
+        }
+
+        public Task<ISnapshot> LoadAsync(string projectionName, IBlobId id, ProjectionVersion version)
+        {
+            return Task.FromResult(Load(projectionName, id, version));
+        }
+
+        public SnapshotMeta LoadMeta(string projectionName, IBlobId id, ProjectionVersion version)
+        {
+            var snapshot = Load(projectionName, id, version);
+            return SnapshotMeta.From(snapshot);
+        }
+
+        public Task<SnapshotMeta> LoadMetaAsync(string projectionName, IBlobId id, ProjectionVersion version)
+        {
+            return Task.FromResult(LoadMeta(projectionName, id, version));
+        }
+
+        public void Save(ISnapshot snapshot, ProjectionVersion version)
+        {
+            if (snapshots.ContainsKey(version) == false)
+                snapshots.TryAdd(version, new List<ISnapshot>());
+
+            snapshots[version].Add(snapshot);
+        }
+    }
+}
