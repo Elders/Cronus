aade7ad7fc3d2b2f572d39d6bf2158c590cfde9f
mynkow <mynkow@gmail.com>
2020-06-15 21:13:28 +0300

    Adds documentation

--- src/Elders.Cronus/Cluster/Job/ICronusJobRunner.cs
+++ src/Elders.Cronus/Cluster/Job/ICronusJobRunner.cs
@@ -4,8 +4,17 @@ using System.Threading.Tasks;
 
 namespace Elders.Cronus.Cluster.Job
 {
+    /// <summary>
+    /// Allows executing a <see cref="ICronusJob{TData}"/> in a cluster environment.
+    /// </summary>
+    /// <remarks>Consider implementing <see cref="IClusterOperations"/> as well.</remarks>
     public interface ICronusJobRunner : IDisposable
     {
+        /// <summary>
+        /// Executes the <see cref="ICronusJob{TData}"/>
+        /// </summary>
+        /// <param name="job">The job.</param>
+        /// <returns>Returns the <see cref="JobExecutionStatus"/> of the job after execution.</returns>
         Task<JobExecutionStatus> ExecuteAsync(ICronusJob<object> job, CancellationToken cancellationToken = default);
     }
 }
