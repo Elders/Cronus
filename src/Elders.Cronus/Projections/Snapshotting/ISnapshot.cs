5267a052cebdbb52705a932b792554ed2d0ebc91
mynkow <mynkow@gmail.com>
2021-03-17 14:01:37 +0200

    fix: Code cleanup

--- src/Elders.Cronus/Projections/Snapshotting/ISnapshot.cs
+++ src/Elders.Cronus/Projections/Snapshotting/ISnapshot.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace Elders.Cronus.Projections.Snapshotting
+﻿namespace Elders.Cronus.Projections.Snapshotting
 {
     public interface ISnapshot
     {
@@ -8,25 +6,5 @@ namespace Elders.Cronus.Projections.Snapshotting
         int Revision { get; }
         object State { get; }
         string ProjectionName { get; }
-    }
-
-    public class SnapshotMeta
-    {
-        SnapshotMeta() { }
-
-        public SnapshotMeta(int revision, string projectionName)
-        {
-            if (revision < -1) throw new ArgumentException($"{revision} must be >= -1", nameof(revision));
-            if (string.IsNullOrEmpty(projectionName)) throw new ArgumentNullException(nameof(projectionName));
-
-            Revision = revision;
-            ProjectionName = projectionName;
-        }
-
-        public int Revision { get; private set; }
-
-        public string ProjectionName { get; private set; }
-
-        public static SnapshotMeta From(ISnapshot snapshot) => new SnapshotMeta(snapshot.Revision, snapshot.ProjectionName);
     }
 }
