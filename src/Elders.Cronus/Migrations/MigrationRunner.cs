1f31fdf48f90db591174ee8ab739a63161549c6f
AlexPlom <alexiosplomaritis@gmail.com>
2021-03-30 11:59:00 +0300

    fix: Stops starting indexes if migrations are enabled

--- src/Elders.Cronus/Migrations/MigrationRunner.cs
+++ src/Elders.Cronus/Migrations/MigrationRunner.cs
@@ -14,13 +14,14 @@ namespace Elders.Cronus.Migrations
         public override void Run(IEnumerable<IMigration<AggregateCommit>> migrations)
         {
             LoadAggregateCommitsResult result = source.LoadAggregateCommits("", 5000);
-
+            int counter = 0;
             try
             {
                 while (result.Commits.Any())
                 {
                     for (int i = 0; i < result.Commits.Count; i++)
                     {
+                        counter++;
                         AggregateCommit sourceCommit = result.Commits[i];
 
                         foreach (var migration in migrations)
@@ -33,6 +34,7 @@ namespace Elders.Cronus.Migrations
                         target.Append(sourceCommit);
                     }
                     result = source.LoadAggregateCommits(result.PaginationToken, 5000); // think of paging
+                    logger.Info(() => $"Migrated commits - Counter: `{counter}` - Pagination Token: `{result.PaginationToken}`");
                 }
             }
             catch (System.Exception ex)
