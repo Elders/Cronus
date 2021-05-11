a6e055f4bdaf4291f469687069fd383f447f7dfb
mynkow <mynkow@gmail.com>
2021-05-11 10:26:44 +0300

    fix: Adds a new method to load aggregate commits

---
+++ src/Elders.Cronus/EventStore/ReplayOptions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Elders.Cronus.EventStore
+{
+    public class ReplayOptions
+    {
+        public ReplayOptions()
+        {
+            AggregateIds = Enumerable.Empty<IAggregateRootId>();
+            ShouldSelect = commit => true;
+        }
+
+        public IEnumerable<IAggregateRootId> AggregateIds { get; set; }
+
+        public string PaginationToken { get; set; }
+
+        public Func<AggregateCommit, bool> ShouldSelect { get; set; }
+
+        public int BatchSize { get; set; } = 1000;
+
+        public DateTimeOffset? After { get; set; }
+
+        public DateTimeOffset? Before { get; set; }
+    }
+}
