085ce752766a97547f8a2d50317afbf659fa0df0
Oleksii Nikolaiev <oleksii.nikolaiev@eldersoss.com>
2022-01-13 13:48:05 +0200

    Add StartAsync that use async rabbitMq implementation

--- src/Elders.Cronus/Hosting/ICronusHost.cs
+++ src/Elders.Cronus/Hosting/ICronusHost.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Elders.Cronus
 {
     public interface ICronusHost : IDisposable
     {
         void Start();
+        Task StartAsync();
         void Stop();
     }
 }
