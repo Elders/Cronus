507111c73c5f7a388ceadca9ec11165f4fd81181
Oleksii Nikolaiev <oleksii.nikolaiev@eldersoss.com>
2022-01-13 14:11:05 +0200

    fix: bump version

--- src/Elders.Cronus/Hosting/CronusHost.cs
+++ src/Elders.Cronus/Hosting/CronusHost.cs
@@ -3,7 +3,6 @@ using Elders.Cronus.Migrations;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace Elders.Cronus
