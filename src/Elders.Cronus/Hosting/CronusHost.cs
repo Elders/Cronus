3f76e8b6cc816d81f070e832449702e984e31f83
leksyCode <oleksii.nikolaiev@eldersoss.com>
2022-05-27 14:48:09 +0300

    feat: Trigger pipeline

--- src/Elders.Cronus/Hosting/CronusHost.cs
+++ src/Elders.Cronus/Hosting/CronusHost.cs
@@ -1,10 +1,10 @@
-﻿using Elders.Cronus.EventStore.Index;
+﻿using System;
+using Elders.Cronus.EventStore.Index;
 using Elders.Cronus.Hosting;
 using Elders.Cronus.Migrations;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using System;
 
 namespace Elders.Cronus
 {
