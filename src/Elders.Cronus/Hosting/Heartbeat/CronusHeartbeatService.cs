b9ff22dbf8cbb7a834449fa64ac536a3e5d3062f
leksyCode <oleksii.nikolaiev@eldersoss.com>
2022-09-14 15:56:16 +0300

    fix: Try to deploy #3

--- src/Elders.Cronus/Hosting/Heartbeat/CronusHeartbeatService.cs
+++ src/Elders.Cronus/Hosting/Heartbeat/CronusHeartbeatService.cs
@@ -1,9 +1,9 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
+﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Elders.Cronus.Hosting.Heartbeat
 {
