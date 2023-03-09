31860e97d9bb802bae4ca0b0e11547fee0e944f1
Stanimir Venkov <stanimir.venkov@eldersoss.com>
2023-03-09 19:28:14 +0200

    Create EveryEvenRevision Snapshot Strategy

--- src/Elders.Cronus/Snapshots/NoSnapshotsStrategy.cs
+++ src/Elders.Cronus/Snapshots/NoSnapshotsStrategy.cs
@@ -9,4 +9,12 @@ namespace Elders.Cronus.Snapshots
             return Task.FromResult(false);
         }
     }
+
+    public sealed class EveryEvenRevisionSnapshotsStrategy : ISnapshotStrategy
+    {
+        public Task<bool> ShouldCreateSnapshotAsync(SnapshotManagerId id, int lastCompletedRevision, int newRevision)
+        {
+            return Task.FromResult(newRevision % 2 == 0);
+        }
+    }
 }
