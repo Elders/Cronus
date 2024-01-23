74ad378ed55a6eaaaf0a8e484c3a53fe8c1508e2
mynkow <mynkow@gmail.com>
2024-01-23 08:14:42 +0200

    fix: Uses tenant singleton migrator for live handling of incomming messages. Otherwise there will be a memory leak

--- src/Elders.Cronus/Migrations/MigrationHandler.cs
+++ src/Elders.Cronus/Migrations/MigrationHandler.cs
@@ -9,9 +9,9 @@ namespace Elders.Cronus.Migrations
     public sealed class MigrationHandler : IMigrationHandler,
         IAggregateCommitHandle<AggregateCommit>
     {
-        private readonly ICronusMigrator cronusMigrator;
+        private readonly ICronusMigratorManual cronusMigrator;
 
-        public MigrationHandler(ICronusMigrator cronusMigrator)
+        public MigrationHandler(ICronusMigratorManual cronusMigrator)
         {
             this.cronusMigrator = cronusMigrator;
         }
