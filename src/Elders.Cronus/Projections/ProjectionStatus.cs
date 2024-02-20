1f555f54e35c34426bec428f5b4b725f4b9c6975
Georgi Shukov <georgi.shukov98@gmail.com>
2024-02-20 16:04:21 +0200

    fix: Properly compares ProjectionStatuses

--- src/Elders.Cronus/Projections/ProjectionStatus.cs
+++ src/Elders.Cronus/Projections/ProjectionStatus.cs
@@ -1,10 +1,13 @@
 ﻿using System.Runtime.Serialization;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
 
 namespace Elders.Cronus.Projections
 {
     [DataContract(Name = "27fdcbb1-d334-473a-b62c-70cc3b6ffdfb")]
-    public class ProjectionStatus : ValueObject<ProjectionStatus>
+    public class ProjectionStatus : IEqualityComparer<ProjectionStatus>, IEquatable<ProjectionStatus>
     {
         ProjectionStatus() { }
 
@@ -87,9 +90,82 @@ namespace Elders.Cronus.Projections
             return status.status;
         }
 
-        public override string ToString()
+        public bool Equals(ProjectionStatus other)
         {
-            return status;
+            if (other is null) return false;
+            if (ReferenceEquals(this, other)) return true;
+
+            if (status == other.status)
+                return true;
+
+            // Everything bellow is for backwords comp
+            if (status == New)
+                return other.status == "replaying" || other.status == "building";
+
+            if (status == "replaying")
+                return other.status == New || other.status == "building";
+
+            if (status == "building")
+                return other.status == New || other.status == "replaying";
+
+            if (status == Fixing)
+                return other.status == "rebuilding";
+
+            if (status == "rebuilding")
+                return other.status == Fixing;
+
+            return false;
+        }
+
+        public bool Equals(ProjectionStatus left, ProjectionStatus right)
+        {
+            if (left is null && right is null) return true;
+            if (left is null)
+                return false;
+            else
+                return left.Equals(right);
+        }
+
+        public int GetHashCode([DisallowNull] ProjectionStatus obj)
+        {
+            return obj.GetHashCode();
+        }
+
+        public static bool operator ==(ProjectionStatus left, ProjectionStatus right)
+        {
+            if (left is null && right is null) return true;
+            if (left is null)
+                return false;
+            else
+                return left.Equals(right);
+        }
+
+        public static bool operator !=(ProjectionStatus left, ProjectionStatus right)
+        {
+            return !(left == right);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null) return false;
+            if (ReferenceEquals(this, obj)) return true;
+
+            return Equals(obj as ProjectionStatus);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int startValue = 2909;
+                int multiplier = 6733;
+
+                int hashCode = startValue;
+
+                hashCode = hashCode * multiplier ^ this.status.GetHashCode();
+
+                return hashCode;
+            }
         }
     }
 }
