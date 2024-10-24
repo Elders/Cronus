ba8b16afc98cc1bba191d067fa80d4318eca935c
Kalina Shupeva <kalina.shupeva@eldersoss.com>
2024-10-24 13:49:10 +0300

    Adds AR to keep track of auto updates

---
+++ src/Elders.Cronus/EventStore/AutoUpdater/Commands/FailAutoUpdate.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization;
+using System;
+
+namespace Elders.Cronus.EventStore.AutoUpdater.Commands;
+
+[DataContract(Name = "fc2cd66d-35a2-4713-ab6e-95c271331531")]
+public class FailAutoUpdate : ISystemCommand
+{
+    FailAutoUpdate() { }
+
+    public FailAutoUpdate(AutoUpdaterId id, DateTimeOffset timestamp)
+    {
+        Id = id;
+        Timestamp = timestamp;
+    }
+
+    [DataMember(Order = 1)]
+    public AutoUpdaterId Id { get; private set; }
+
+    [DataMember(Order = 2)]
+    public DateTimeOffset Timestamp { get; private set; }
+}
---
+++ src/Elders.Cronus/EventStore/AutoUpdater/Events/AutoUpdateTriggered.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Elders.Cronus.EventStore.AutoUpdater.Events;
+
+[DataContract(Name = "9c3a9f5c-0a91-4b1d-82d1-4f3409913046")]
+public class AutoUpdateTriggered : ISystemEvent
+{
+    AutoUpdateTriggered() { }
+
+    public AutoUpdateTriggered(AutoUpdaterId id, uint previousVersion , uint currentVersion, string boundedContext, DateTimeOffset timestamp)
+    {
+        Id = id;
+        BoundedContext = boundedContext;
+        PreviousVersion = previousVersion;
+        CurrentVersion = currentVersion;
+        Timestamp = timestamp;
+    }
+
+    [DataMember(Order = 1)]
+    public AutoUpdaterId Id { get; private set; }
+
+    [DataMember(Order = 2)]
+    public uint PreviousVersion { get; private set; }
+
+    [DataMember(Order = 3)]
+    public uint CurrentVersion { get; private set; }
+
+    [DataMember(Order = 4)]
+    public string BoundedContext { get; private set; }
+
+    [DataMember(Order = 5)]
+    public DateTimeOffset Timestamp { get; private set; }
+}
